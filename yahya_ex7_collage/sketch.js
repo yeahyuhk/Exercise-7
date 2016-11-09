ae374bdaa03562ea01fb14a5f04ad2070be0a65d
Yahya Khan <yahyas.email@gmail.com>
2016-11-09 01:35:34 -0500

    Add files via upload

--- yahya_ex7_collage/sketch.js
+++ yahya_ex7_collage/sketch.js
@@ -1,42 +1,43 @@
 var sun, cloud, tree, pumpkin, moon, treedead;
 var seasons = ['summer', 'fall', 'winter', 'spring'];
 var clouds = [];
-var cuecloudz = true;
 
 function preload() {
   sun = loadImage('data/sun.png');
+  moon = loadImage('data/moon.png');
   cloud = loadImage('data/cloud[1].png');
   pumpkin = loadImage('data/pumpkin.png');
-  tree = loadImage('data/Pine_Tree[1].png');
-  treedead = loadImage('data/tree-dead.png');
+  tree1 = loadImage('data/tree-live.png');
+  tree3 = loadImage('data/Pine_Tree[1].png');
+  tree2 = loadImage('data/tree-dead.png');
 
 }
 
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(700, 600);
   background(0);
 }
 
 function draw() {
 
-  background(100);
+  background(120, 180, 250);
 
-  image(treedead, 100, 100);
-  if (frameCount % 120 === 0) {
-    cloudz();
-    for (var i = 0; i < cloudz.length; i++) {
+  rect(0, 400, width, height);
 
-      cloudz[i].run();
-      if (cloudz[i].isDead()) {
-        cloudz.splice(i, 1);
-      }
-    }
-  }
+
+  //ground
+  noStroke();
+  fill(50, 200, 100);
+
+
+
+  image(tree1, 100, 100);
 }
 
-function cloudz() {
-  for (var i = 0; i < 15; i += 5) {
-    var b = new image(cloud, createVector(0, 5));
-    clouds.push(b);
+function tree(counter){
+  if counter = 1{
+    
   }
+  
+  
 }
